dbe0e7b0002ce5abbbb051b23433e6d4f5606e03
gqtqulin <gqtqulin@icloud.com>
2023-08-22 13:58:39 +0300

    add rnd resize

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -2,42 +2,51 @@ import { useState, useEffect, useRef } from "react";
 import styles from "./Note.module.css";
 import { Rnd } from "react-rnd";
 
-const Note = ({ setIsEditMode, notes, setNotes, currentId, note, id}) => {
+const Note = ({ setIsEditMode, notes, setNotes, currentId, note, id }) => {
   const [noteName, setNoteName] = useState(note.title);
   const [noteText, setNote] = useState(note.note);
 
-  // попробовать сделать сохранение с помощью useEffect без нажатия по кнопке
-  const handleSubmitForm = (e) => {
-    e.preventDefault();
+  useEffect(() => {
     const newNotes = { ...notes };
     console.log(newNotes);
     const updatedNote = { title: noteName, note: noteText };
     newNotes[id] = updatedNote;
     setNotes(newNotes);
-  };
+  }, [noteName, noteText]);
 
   return (
-    <div className={styles.noteContainer}>
-      <form className={styles.noteForm} onSubmit={handleSubmitForm}>
-        <div className={styles.restForm}>
-          <input
-            type="text"
-            value={noteName}
-            placeholder="note name 31337"
-            className={styles.noteNameInput}
-            onChange={(e) => setNoteName(e.target.value)}
-          ></input>
-          <input className={styles.button} value="Save" type="submit" />
-        </div>
-        <div className={styles.textAreaContainer}>
-          <textarea
-            className={styles.textArea}
-            value={noteText}
-            onChange={(e) => setNote(e.target.value)}
-          />
-        </div>
+    <Rnd
+      default={{
+        x: 0,
+        y: 0,
+        width: 200,
+      }}
+      minWidth={30}
+      enableResizing={{
+        bottom: false,
+        left: false,
+        right: false,
+        top: false,
+        bottomLeft: true,
+        bottomRight: true,
+      }}
+      scale={1}
+    >
+      <form className={styles.noteForm}>
+        <input
+          type="text"
+          value={noteName}
+          placeholder="note name 31337"
+          className={styles.noteNameInput}
+          onChange={(e) => setNoteName(e.target.value)}
+        ></input>
+        <textarea
+          className={styles.textArea}
+          value={noteText}
+          onChange={(e) => setNote(e.target.value)}
+        />
       </form>
-    </div>
+    </Rnd>
   );
 };
 
